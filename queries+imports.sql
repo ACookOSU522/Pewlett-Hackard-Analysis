d087da28ebff52f3d361fbef43810dee9b4049a2
Alison Cook <Crlygrly0182@msn.com>
2022-07-27 19:51:30 -0400

    adding SQL Mod 7 challenge items.

--- queries+imports.sql
+++ queries+imports.sql
@@ -124,3 +124,275 @@ INTO retirement_info
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Creating tables for PH-EmployeeDB
+CREATE TABLE departments (
+     dept_no VARCHAR(4) NOT NULL,
+     dept_name VARCHAR(40) NOT NULL,
+     PRIMARY KEY (dept_no),
+     UNIQUE (dept_name)
+);
+-- Creating tables for PH-EmployeeDB pt 2
+Create Table employees ( 
+	emp_no INT NOT NULL,
+     birth_date DATE NOT NULL,
+     first_name VARCHAR NOT NULL,
+     last_name VARCHAR NOT NULL,
+     gender VARCHAR NOT NULL,
+     hire_date DATE NOT NULL,
+     PRIMARY KEY (emp_no)
+);
+-- Creating tables for PH-EmployeeDB pt 3	 
+CREATE TABLE dept_manager (
+dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no));	
+-- Creating tables for PH-EmployeeDB pt 4
+CREATE TABLE salaries (
+  emp_no INT NOT NULL,
+  salary INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no));
+ -- Creating tables for PH-EmployeeDB pt 5
+ CREATE TABLE titles (
+  emp_no INT NOT NULL,
+  title varchar,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  PRIMARY KEY (emp_no));
+ -- Creating tables for PH-EmployeeDB pt 6
+ CREATE TABLE dept_emp (
+  emp_no INT NOT NULL,
+  dept_no VARCHAR(4) NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no, dept_no));
+  
+  DROP TABLE titles;
+  
+ --recreate title table
+ CREATE TABLE IF NOT EXISTS public.titles
+(
+    emp_no integer NOT NULL,
+    title character varying COLLATE pg_catalog."default" NOT NULL,
+    from_date date NOT NULL,
+    to_date date NOT NULL,
+    CONSTRAINT pk_titles PRIMARY KEY (emp_no, from_date, to_date),
+    CONSTRAINT fk_titles_emp_no FOREIGN KEY (emp_no)
+        REFERENCES public.employees (emp_no) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+DROP table dept_emp 
+
+CREATE TABLE IF NOT EXISTS public.dept_emp
+(
+    emp_no integer NOT NULL,
+    dept_no character varying COLLATE pg_catalog."default" NOT NULL,
+    from_date date NOT NULL,
+    to_date date NOT NULL,
+    CONSTRAINT pk_dept_emp PRIMARY KEY (emp_no, dept_no, from_date, to_date),
+    CONSTRAINT fk_dept_emp_dept_no FOREIGN KEY (dept_no)
+        REFERENCES public.departments (dept_no) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT fk_dept_emp_emp_no FOREIGN KEY (emp_no)
+        REFERENCES public.employees (emp_no) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+);
+
+  SELECT * FROM departments;
+  SELECT * FROM departments;
+
+  --Retirement eligibility by year
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+--1952
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+--1953
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+--1954
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
+--1955
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Retirement eligibility COUNT
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--create new table
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no;
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT emp_no,
+    first_name,
+last_name,
+    gender
+
+SELECT emp_no,
+    first_name,
+last_name,
+    gender
+	INTO emp_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT e.emp_no,
+    e.first_name,
+e.last_name,
+    e.gender,
+    s.salary,
+    de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	      AND (de.to_date = '9999-01-01');
+		  
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);	
+		
+
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);		
+		
+		
+-- Use Dictinct with Orderby to remove duplicate rows
+--SELECT DISTINCT ON (______) _____,
+--______,
+--______,
+--______
+
+--INTO nameyourtable
+--FROM _______
+--WHERE _______
+--ORDER BY _____, _____ DESC;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (e.emp_no) 
+e.emp_no,
+e.first_name,
+e.last_name,
+ti.title,
+ti.from_date,
+ti.to_date
+
+
+INTO retirement_titles
+FROM Employees as e 
+inner join titles as ti 
+on (e.emp_no = ti.emp_no)
+WHERE e.birth_date between '1952-01-01' and '1955-12-31'
+ORDER BY e.emp_no ASC, ti.to_date DESC;
+
+select * from retirement_titles
+------------------------------------------------------------
+SELECT DISTINCT ON (r.emp_no)
+r.emp_no,
+r.first_name,
+r.last_name,
+r.title
+
+INTO unique_titles
+FROM retirement_titles as r
+WHERE r.to_date = '9999-01-01'
+ORDER BY r.emp_no ASC, r.to_date DESC
+------------------------------------------------------------
+SELECT COUNT (title), title
+INTO retiring_titles
+FROM unique_titles 
+GROUP BY title
+ORDER BY 1 DESC
+-----------------------------------------------------------
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+d.from_date,
+d.to_date,
+ti.title
+
+--into mentorship_eligibility
+FROM Employees as e
+inner join dept_emp as d
+on (e.emp_no = d.emp_no)
+inner join titles as ti
+on (e.emp_no = ti.emp_no)
+WHERE d.to_date = '9999-01-01' 
+and e.birth_date between '1965-01-01' and '1965-12-31'
+ORDER BY e.emp_no ASC, ti.to_date DESC
+
+
+select * from unique_titles
